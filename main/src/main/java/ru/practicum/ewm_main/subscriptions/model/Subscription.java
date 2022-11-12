e0dc7afc7747e95078a6dbd4fe77d67fdcd116e2
Mikhail <ezar4992@yandex.ru>
2022-11-12 14:55:03 +0400

    <fix><фикс по ревью>

--- main/src/main/java/ru/practicum/ewm_main/subscriptions/model/Subscription.java
+++ main/src/main/java/ru/practicum/ewm_main/subscriptions/model/Subscription.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 public class Subscription {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "subscription_id")
     private Long id;
     @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "subscriber_id")
--- main/src/main/java/ru/practicum/ewm_main/subscriptions/service/SubscriptionServiceImpl.java
+++ main/src/main/java/ru/practicum/ewm_main/subscriptions/service/SubscriptionServiceImpl.java
@@ -8,6 +8,7 @@ import ru.practicum.ewm_main.events.model.dto.EventShortDto;
 import ru.practicum.ewm_main.events.repository.EventRepository;
 import ru.practicum.ewm_main.exception.NotFoundException;
 import ru.practicum.ewm_main.exception.ValidationException;
+import ru.practicum.ewm_main.requests.model.Request;
 import ru.practicum.ewm_main.requests.repository.RequestRepository;
 import ru.practicum.ewm_main.stats.StatService;
 import ru.practicum.ewm_main.subscriptions.model.Subscription;
@@ -21,6 +22,8 @@ import ru.practicum.ewm_main.users.model.dto.UserShortDto;
 import ru.practicum.ewm_main.users.repository.UserRepository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -100,8 +103,9 @@ public class SubscriptionServiceImpl {
         User user = userRepository.findById(id).orElseThrow(() ->
                 new NotFoundException("Пользователь с таким id не найден!"));
         List<Event> eventList = eventRepository.getEventsBySubscriber(id, SubscriptionStatus.CONFIRMED, LocalDateTime.now());
+        Map<Long, Long> confirmedMap = getConfirmedRequestsList(eventList);
         Map<Long, Long> viewsStat = statService.getViews(eventList, false);
-        return eventList.stream().map(e -> EventMapper.toEventShortDto(e, requestRepository.getConfirmedRequests(e.getId()),
+        return eventList.stream().map(e -> EventMapper.toEventShortDto(e, confirmedMap.getOrDefault(e.getId(), 0L),
                 viewsStat.getOrDefault(e.getId(), 0L))).collect(Collectors.toList());
     }
 
@@ -116,6 +120,22 @@ public class SubscriptionServiceImpl {
                 new NotFoundException("Пользователь с таким id не найден!"));
         return userRepository.getSubscriptions(id).stream().map(UserMapper::toUserShortDto).collect(Collectors.toList());
 
+    }
+
+    private Map<Long, Long> getConfirmedRequestsList(List<Event> eventList) {
+        List<Long> ids = new ArrayList<>();
+
+        for (Event event : eventList) {
+            ids.add(event.getId());
+        }
+
+        List<Request> requestList = requestRepository.getConfirmedRequestsList(ids);
+        Map<Long, Long> requestMap = new HashMap<>();
+        for (Request request : requestList) {
+            requestMap.put(request.getEvent().getId(), requestMap.getOrDefault(request.getEvent().getId(), 0L) + 1L);
+        }
+
+        return requestMap;
     }
 
 
